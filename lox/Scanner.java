55c4faaf9265842ec66d571e8036fb960985396b
Ricardo Díaz <ricardotk002@gmail.com>
2026-06-16 03:00:26 -0400

    Support double-char lexemes

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -43,6 +43,19 @@ class Scanner {
       case ';': addToken(SEMI_COLON); break;
       case '*': addToken(STAR); break;
 
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+
       default:
         Lox.error(line, "Unexpected character.");
         break;
@@ -51,6 +64,14 @@ class Scanner {
 
   private char advance() {
     return source.charAt(current++);
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+    
+    current++;
+    return true;
   }
 
   private void addToken(TokenType type) {
