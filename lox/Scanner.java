a89cc5fec20f683bdab2a16fd9945b62c96c6add
Ricardo Díaz <ricardotk002@gmail.com>
2026-06-16 03:23:12 -0400

    Scan numbers

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -76,7 +76,12 @@ class Scanner {
       case '"': string(); break;
 
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
+        
         break;
     }
   }
@@ -88,6 +93,11 @@ class Scanner {
   private char peek() {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
   }
 
   private boolean match(char expected) {
@@ -113,6 +123,22 @@ class Scanner {
 
     String value = source.substring(start + 1, current - 1);
     addToken(STRING, value);
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
   private void addToken(TokenType type) {
