310f501ef9cc350ebbc9f1d9847c8763ea25ca98
Ricardo Díaz <ricardotk002@gmail.com>
2026-06-18 01:59:07 -0400

    Support while

--- lox/Parser.java
+++ lox/Parser.java
@@ -51,6 +51,7 @@ class Parser {
   private Stmt statement() {
     if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
+    if (match(WHILE)) return whileStatement();
     if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
     return expressionStatement();
@@ -75,6 +76,15 @@ class Parser {
     Expr value = expression();
     consume(SEMI_COLON, "Expect ';' after value.");
     return new Stmt.Print(value);
+  }
+
+  private Stmt whileStatement() {
+    consume(LEFT_PAREN, "Expect '(' after while.");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after condition.");
+    Stmt body = statement();
+
+    return new Stmt.While(condition, body);
   }
 
   private List<Stmt> block() {
