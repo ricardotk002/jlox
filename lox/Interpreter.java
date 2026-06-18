5e625c7a4202eba83eaacc2afcecdd8f836491d3
Ricardo Díaz <ricardotk002@gmail.com>
2026-06-18 01:42:06 -0400

    Add support for logical conditions

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -144,6 +144,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     return null;
   }
 
+  @Override
+  public Object visitLogicalExpr(Expr.Logical expr) {
+    Object left = evaluate(expr.left);
+
+    if (expr.operator.type == TokenType.OR) {
+      if (isTruthy(left)) return left;
+    } else {
+      if (!isTruthy(left)) return left;
+    }
+
+    return evaluate(expr.right);
+  }
+
   private void execute(Stmt stmt) {
     stmt.accept(this);
   }
